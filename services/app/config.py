f14afc73b6e85708d637f24f2f7606ea5af586df
Thejas027 <teju270934@gmail.com>
2025-06-02 15:57:27 +0530

    there was a missing req added tjhat

--- services/app/config.py
+++ services/app/config.py
@@ -3,4 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GROQ_API_KEY_FROM_ENV = os.getenv("GROQ_API_KEY")
+# DEBUG LINE
+print(
+    f"!!!!!!!!!!!! DEBUG: In config.py, GROQ_API_KEY_FROM_ENV is: '{GROQ_API_KEY_FROM_ENV}' !!!!!!!!!!!!")
+GROQ_API_KEY = GROQ_API_KEY_FROM_ENV
