73b69b273fb76524fb3f562e26ddc299ba2f6663
Thejas027 <teju270934@gmail.com>
2025-06-03 13:15:11 +0530

    updated the ml config file

--- services/app/config.py
+++ services/app/config.py
@@ -4,10 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GROQ_API_KEY_FROM_ENV = os.getenv("GROQ_API_KEY")
-# DEBUG LINE
-print(
-    f"!!!!!!!!!!!! DEBUG: In config.py, GROQ_API_KEY_FROM_ENV is: '{GROQ_API_KEY_FROM_ENV}' !!!!!!!!!!!!")
-GROQ_API_KEY = GROQ_API_KEY_FROM_ENV
+# ...existing code...
+# (DEBUG LINE REMOVED)
+# ...existing code...
 
 # Load all GROQ API keys from environment variables
 GROQ_API_KEYS = [
