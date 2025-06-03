20920c26b88a851bc789a9fc3d4541be22ff928a
Thejas027 <teju270934@gmail.com>
2025-06-03 12:23:05 +0530

    added the ui for multi pdf summary

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
 import PdfSummarizer from "./components/PdfSummarizer"; // Added
+import MultiPdfSummarizer from "./components/MultiPdfSummarizer";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-      <PdfSummarizer />
+    <div className="min-h-screen bg-gradient-to-br from-indigo-100 to-purple-100 py-6 flex flex-col justify-center sm:py-12">
+      <div className="flex flex-col md:flex-row gap-8 justify-center items-start w-full max-w-6xl mx-auto">
+        <div className="w-full md:w-1/2">
+          <PdfSummarizer />
+        </div>
+        <div className="w-full md:w-1/2">
+          <MultiPdfSummarizer />
+        </div>
+      </div>
     </div>
   );
 }
